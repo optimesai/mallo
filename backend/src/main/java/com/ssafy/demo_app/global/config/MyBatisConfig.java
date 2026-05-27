c114f44b9792dabd65229719e5cfdec16e18c3d2
inley <inley@naver.com>
2026-05-27 14:53:37 +0900

    chore: 사용하지 않는 파일 정리

--- backend/src/main/java/com/ssafy/demo_app/global/config/MyBatisConfig.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.demo_app.global.config;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan("com.ssafy.**.mapper")
-public class MyBatisConfig {
-}
